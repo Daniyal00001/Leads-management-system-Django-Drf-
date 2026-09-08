edf0966af7cd86bf3cb121db780a6913b27ac4a8
Muhammad Daniyal Tallat <daniyaltallat0@gmail.com>
2026-09-08 19:16:48 +0500

    configure admin settings

--- apps/leads/admin.py
+++ apps/leads/admin.py
@@ -1,3 +1,159 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Lead,
+    Phase,
+    PhaseManagerHistory,
+    PhaseEngineer,
+    PhaseEngineerHistory,
+)
+
+
+class PhaseInline(admin.TabularInline):
+    model = Phase
+    extra = 0
+    fields = [
+        "order",
+        "type",
+        "current_manager",
+        "status",
+        "start_date",
+        "due_date",
+    ]
+    show_change_link = True
+
+
+@admin.register(Lead)
+class LeadAdmin(admin.ModelAdmin):
+    list_display = [
+        "project_name",
+        "client_name",
+        "status",
+        "created_by",
+        "created_at",
+    ]
+
+    list_filter = [
+        "status",
+        "test_type",
+        "created_at",
+    ]
+
+    search_fields = [
+        "project_name",
+        "client_name",
+        "client_email",
+    ]
+
+    readonly_fields = [
+        "sale_decided_at",
+        "created_at",
+        "updated_at",
+    ]
+
+    inlines = [PhaseInline]
+
+    def save_model(self, request, obj, form, change): # here created by cannot be null ....so , created_by = logged-in user...
+        if not obj.pk:
+            obj.created_by = request.user
+
+        super().save_model(request, obj, form, change)
+
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+
+        for instance in instances:
+            if isinstance(instance, Phase) and not instance.pk:
+                instance.created_by = request.user
+
+            instance.save()
+
+        for obj in formset.deleted_objects:
+            obj.delete()
+
+        formset.save_m2m()
+
+
+class PhaseManagerHistoryInline(admin.TabularInline):
+    model = PhaseManagerHistory
+    extra = 0
+
+    readonly_fields = [
+        "manager",
+        "action",
+        "comment",
+        "performed_by",
+        "action_at",
+    ]
+
+    can_delete = False
+
+
+class PhaseEngineerInline(admin.TabularInline):
+    model = PhaseEngineer
+    extra = 0
+
+    fields = [
+        "engineer",
+        "status",
+        "assigned_by",
+    ]
+
+
+@admin.register(Phase)
+class PhaseAdmin(admin.ModelAdmin):
+    list_display = [
+        "lead",
+        "order",
+        "type",
+        "current_manager",
+        "status",
+        "due_date",
+    ]
+
+    list_filter = [
+        "status",
+        "type",
+    ]
+
+    search_fields = [
+        "lead__project_name",
+    ]
+
+    inlines = [
+        PhaseManagerHistoryInline,
+        PhaseEngineerInline,
+    ]
+
+    def save_formset(self, request, form, formset, change): # here created by cannot be null ....so , created_by = logged-in user...
+        instances = formset.save(commit=False)
+
+        for instance in instances:
+            if isinstance(instance, PhaseEngineer) and not instance.pk:
+                instance.assigned_by = request.user
+
+            instance.save()
+
+        for obj in formset.deleted_objects:
+            obj.delete()
+
+        formset.save_m2m()
+
+
+@admin.register(PhaseEngineerHistory)
+class PhaseEngineerHistoryAdmin(admin.ModelAdmin):
+    list_display = [
+        "phase",
+        "engineer",
+        "action",
+        "performed_by",
+        "action_at",
+    ]
+
+    list_filter = [
+        "action",
+    ]
+
+    readonly_fields = [
+        "action_at",
+    ]
--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -1,3 +1,40 @@
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+from apps.core.models import TimeStampedModel
+
+
+class NotificationType(models.TextChoices):
+    PHASE_ASSIGNED = "phase_assigned", "Phase Assigned"
+    PHASE_ACCEPTED = "phase_accepted", "Phase Accepted"
+    PHASE_DECLINED = "phase_declined", "Phase Declined"
+    PHASE_COMPLETED = "phase_completed", "Phase Completed"
+    LEAD_SALE = "lead_sale", "Lead Marked as Sale"
+    PROJECT_ASSIGNED = "project_assigned", "Project Assigned"
+
+
+class Notification(TimeStampedModel):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="notifications",
+    )
+    type = models.CharField(max_length=30, choices=NotificationType.choices)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True, blank=True)
+    object_id = models.PositiveIntegerField(null=True, blank=True)
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["user", "is_read"]),
+        ]
+
+    def __str__(self):
+        return f"[{self.type}] to {self.user}"
